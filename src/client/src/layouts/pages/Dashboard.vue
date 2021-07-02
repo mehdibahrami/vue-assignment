c4905480070943234ac8351ef2ed4644ef14eebd
Mehdi Bahrami <mehdibahraami@gmail.com>
2021-07-02 23:45:23 +0430

    feat: add speed and soc gauge charts to dashboard

--- src/client/src/layouts/pages/Dashboard.vue
+++ src/client/src/layouts/pages/Dashboard.vue
@@ -14,23 +14,73 @@
           </Card>
         </div>
         <!-- Right column -->
-        <div class="grid-cols-1"></div>
+        <div class="grid-cols-1">
+          <!-- Column grid -->
+          <div class="grid-cols-1 grid-lg-cols-2 grid-sm-cols-2">
+            <!-- Left column - Speed -->
+            <Card title="Speed">
+              <GaugeChart
+                :data="data.speed"
+                :colors="speedGaugeChartColors"
+                label="km/h"
+              />
+            </Card>
+            <!-- Right column - SoC -->
+            <Card title="State of Charge">
+              <GaugeChart
+                :data="data.soc"
+                :colors="socGaugeChartColors"
+                label="%"
+              />
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import { Card, LeafletMap } from "@/components";
+import { Card, LeafletMap, GaugeChart } from "@/components";
+import { MID_CHARGE, LOW_CHARGE, SPEED_LIMIT } from "@/constants";
 
 export default {
   name: "Dashboard",
   components: {
     Card,
     LeafletMap,
+    GaugeChart,
   },
   props: {
     data: Object,
+  },
+  computed: {
+    speedGaugeChartColors: function () {
+      // Set speed guage chart color based on speed value
+      return [
+        function ({ value }) {
+          if (value <= SPEED_LIMIT) {
+            return "#7ca363";
+          } else {
+            return "#AD6D6D";
+          }
+        },
+      ];
+    },
+    socGaugeChartColors: function () {
+      // Set soc guage chart color based on soc value
+      return [
+        function ({ value }) {
+          if (value <= LOW_CHARGE) {
+            return "#AD6D6D";
+          } else if (value > LOW_CHARGE && value <= MID_CHARGE) {
+            return "#C38540";
+          } else {
+            return "#7ca363";
+          }
+        },
+      ];
+    },
   },
 };
 </script>
