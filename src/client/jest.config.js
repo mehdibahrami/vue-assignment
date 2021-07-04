591dc9df4919254d12cd95f24f82e1bb555e6f6d
Mehdi Bahrami <mehdibahraami@gmail.com>
2021-07-04 23:15:56 +0430

    chore: add new patterns to jest config

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
+  moduleFileExtensions: ["js", "vue", "json"],
+  verbose: true,
+  moduleDirectories: ["node_modules", "src"],
+  modulePaths: ["<rootDir>/src", "<rootDir>/node_modules"],
+  transformIgnorePatterns: ["/node_modules/(?!leaflet)"],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
 };
