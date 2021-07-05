134c6202343a3809e1973da25b01dd1386f6cefe
Mehdi Bahrami <mehdibahraami@gmail.com>
2021-07-05 08:46:44 +0430

    refactor(dashboard): move all titles and units of cards and charts to the constants data property

--- src/client/src/layouts/pages/Dashboard.vue
+++ src/client/src/layouts/pages/Dashboard.vue
@@ -18,32 +18,40 @@
           <!-- Column grid -->
           <div class="grid-cols-1 grid-lg-cols-2 grid-sm-cols-2">
             <!-- Left column - Speed gauge chart -->
-            <Card title="Speed">
+            <Card :title="constants.speedGaugeChart.cardTitle">
               <GaugeChart
                 :data="data.speed"
                 :options="speedGaugeChartOptions"
               />
             </Card>
-            <!-- Right column - SoC -->
-            <Card title="State of Charge">
+            <!-- Right column - SoC gauge chart -->
+            <Card :title="constants.socGaugeChart.cardTitle">
               <GaugeChart :data="data.soc" :options="socGaugeChartOptions" />
             </Card>
           </div>
           <!-- Column grid -->
           <div class="grid-cols-1 grid-lg-cols-2 grid-sm-cols-2">
             <!-- Left column - Energy -->
-            <StatisticsCard title="Energy" :statistic="data.energy" unit="kW" />
+            <StatisticsCard
+              :title="constants.energyStatisticsCard.cardTitle"
+              :statistic="data.energy"
+              :unit="constants.energyStatisticsCard.cardUnit"
+            />
             <!-- Right column - Odometer -->
-            <StatisticsCard title="Odometer" :statistic="data.odo" unit="km" />
+            <StatisticsCard
+              :title="constants.odometerStatisticsCard.cardTitle"
+              :statistic="data.odo"
+              :unit="constants.odometerStatisticsCard.cardUnit"
+            />
           </div>
         </div>
       </div>
       <!-- Column grid -->
       <div class="grid-cols-1 grid-lg-cols-2 grid-sm-cols-2 grid-top-margin">
         <!-- Left column - Speed line chart -->
-        <Card title="Speed (km/h)">
+        <Card :title="constants.speedLineChart.cardTitle">
           <LineChart
-            title="Speed"
+            :title="constants.speedLineChart.chartTitle"
             :data="{ x: data.time, y: data.speed }"
             :options="speedLineChartOptions"
             :interval="speedLineChartInterval"
@@ -51,10 +59,10 @@
           />
         </Card>
         <!-- Right column - SoC line chart -->
-        <Card title="State of Charge (%)">
+        <Card :title="constants.socLineChart.cardTitle">
           <LineChart
-            type="area"
-            title="Speed"
+            :type="constants.socLineChart.chartType"
+            :title="constants.socLineChart.chartTitle"
             :data="{ x: data.time, y: data.soc }"
             :options="socLineChartOptions"
             :interval="speedLineChartInterval"
@@ -109,6 +117,31 @@ export default {
       setSocLineChartColor,
       speedLineChartInterval: SPEED_LINE_CHART_INTERVAL,
       socLineChartInterval: SOC_LINE_CHART_INTERVAL,
+      constants: {
+        speedGaugeChart: {
+          cardTitle: "Speed",
+        },
+        socGaugeChart: {
+          cardTitle: "State of Charge",
+        },
+        energyStatisticsCard: {
+          cardTitle: "Energy",
+          cardUnit: "kW",
+        },
+        odometerStatisticsCard: {
+          cardTitle: "Odometer",
+          cardUnit: "km",
+        },
+        speedLineChart: {
+          cardTitle: "Speed (km/h)",
+          chartTitle: "Speed",
+        },
+        socLineChart: {
+          cardTitle: "State of Charge (%)",
+          chartTitle: "SoC",
+          chartType: "area",
+        },
+      },
     };
   },
 };
